53cfdd98207f0a0843e5352b1b9260c50b82c04c
Rubi <pathanrubiya269@gmail.com>
2026-02-17 08:53:34 +0530

    terraform

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "roboshop" {
   ami           = data.aws_ami.joindevops.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.allow-all.id ]
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "allow-all" {
+resource "aws_security_group" "allow_all" {
     name        = "allow-all"
     description = "allow all traffic"
 
--- secure-state/provider.tf
+++ secure-state/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.98.0"
+      version = "6.27.0"
     }
   }
 
   backend "s3" {
-    bucket = "secure-state-test"
+    bucket = "84s1-remote-state"
     key    = "secure"
     region = "us-east-1"
     encrypt        = true
